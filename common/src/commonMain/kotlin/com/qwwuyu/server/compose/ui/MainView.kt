b8a0b67ca58fac64eae061903a7b2bd7943cce87
qwwuyu <qiweiwuyu@gmail.com>
2021-10-14 19:08:40 +0800

    study

--- common/src/commonMain/kotlin/com/qwwuyu/server/compose/ui/MainView.kt
+++ common/src/commonMain/kotlin/com/qwwuyu/server/compose/ui/MainView.kt
@@ -1,15 +1,50 @@
 package com.qwwuyu.server.compose.ui
 
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.selection.DisableSelection
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.qwwuyu.server.compose.theme.WidgetGalleryTheme
+import kotlinx.coroutines.launch
 
 @Composable
 fun MainView() {
     DisableSelection {
         WidgetGalleryTheme {
-            Text("demo")
+            val scaffoldState = rememberScaffoldState()
+            Scaffold(
+                scaffoldState = scaffoldState,
+                modifier = Modifier,
+                drawerContent = {
+                    MainDrawer()
+                }
+            ) {
+                val scope = rememberCoroutineScope()
+                MainContent(
+                    openDrawer = {
+                        scope.launch {
+                            scaffoldState.drawerState.open()
+                        }
+                    }
+                )
+            }
         }
     }
+}
+
+@Composable
+fun MainDrawer() {
+    Text(
+        "MainDrawer", modifier = Modifier.width(200.dp)
+    )
+}
+
+@Composable
+fun MainContent(openDrawer: () -> Unit) {
+    Text("MainContent")
 }
--- desktop/src/jvmMain/kotlin/com/qwwuyu/server/main.kt
+++ desktop/src/jvmMain/kotlin/com/qwwuyu/server/main.kt
@@ -1,16 +1,19 @@
 package com.qwwuyu.server
 
+import androidx.compose.ui.graphics.painter.BitmapPainter
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowSize
 import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
+import com.qwwuyu.server.compose.platform.imageResource
 import com.qwwuyu.server.compose.ui.MainView
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "qwwuyu",
+        icon = BitmapPainter(imageResource("drawable-nodpi/icon.png")),
         state = WindowState(size = WindowSize(800.dp, 600.dp))
     ) {
         MainView()
