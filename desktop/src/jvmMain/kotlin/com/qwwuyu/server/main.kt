527806cbc983399ad93bd0524e214a2341e03ec2
qwwuyu <qiweiwuyu@gmail.com>
2021-11-02 23:21:25 +0800

    study

--- desktop/src/jvmMain/kotlin/com/qwwuyu/server/main.kt
+++ desktop/src/jvmMain/kotlin/com/qwwuyu/server/main.kt
@@ -6,13 +6,13 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.selection.DisableSelection
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.RenderVectorGroup
-import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -22,7 +22,6 @@ import com.qwwuyu.server.compose.nav.LocalNav
 import com.qwwuyu.server.compose.nav.Nav
 import com.qwwuyu.server.compose.theme.AppTheme
 import com.qwwuyu.server.compose.utils.WLog
-import kotlinx.coroutines.launch
 
 fun main() = application {
     Window(
